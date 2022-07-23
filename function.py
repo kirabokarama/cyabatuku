a04195e049fabafc136a62936f71d0ec563a6cb1
kellen1 <mbabazikellen2016@gmail.com>
2022-07-23 04:25:38 +0200

    this my work

---
+++ function.py
@@ -0,0 +1,19 @@
+#defining functionin py
+#def test(message) 
+#print(message)
+# calling function
+#test("this is python")
+#test("hello function")
+def summ(num):
+
+    sum= 0
+
+    for i in num:
+
+         sum+= i
+
+    return sum
+
+print(summ((list)))
+
+list=[1,2,3,4]
